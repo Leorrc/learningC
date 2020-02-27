37df5e1b7ca4e7b3af8ce9b93f30fbbad03d92d2
leocarneiro <leoramos_10@hotmail.com>
2020-02-26 22:17:32 -0300

    I get to print abc with width

--- ft_printf/main.c
+++ ft_printf/main.c
@@ -1,26 +1,82 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <stdarg.h>
+#include <unistd.h>
 
-char  minus_flag(const char *format, char *arg)
+void  ft_putstr(char *s)
+{
+  while(*s)
+  {
+    write(1, s, 1);
+    s++;
+  }
+}
 
 
 int   ft_printf(const char *format, ...)
 {
-  va_list arg;
-  int     i;
+  va_list   arg;
+  char      *fields;
+  char      *s;
+  int       i;
+  int       j;
+  int       len;
+  int       len2;
 
   va_start(arg, format);
+  s = va_arg(arg, char *);
+  /*
+  fields = (char *)malloc(10);
+  //%[flags][width][.precision][size]type
   while (format[i])
   {
     if (format[i] != '%')
-      printf("%c", format[i]);
+      write(1, &format[i], 1);
     else
     {
       i++;
-      
-
+      while (format[i] == '-')
+        fields[0] = '-';
+      while (format[i] >= '0' && format[i] <= '9')
+        fields[1] = format[i];
+      while (format[i] == 's')
+        fields[3] = '-';
     }
+    i++;
+  }
+  len = 10;
+  i = 0;
+  while (i < len)
+  {
+    while (s)
+      write(1, &s[i], 1);
+    write(1, " ", 1);
+    i++;
+  }
+  */
+  while (format[i])
+  {
+    if (format[i] != '%')
+      write(1, &format[i], 1);
+    else
+    {
+      i++;
+      if (format[i] == '5' && format[i + 1] == 's')
+      {
+        len = atoi(&format[i]);
+        len2 = strlen(s);
+        j = 0;
+        while (j < (len - len2))
+        {
+          write(1, " ", 1);
+          j++;
+        }
+        ft_putstr(s);
+        i++;
+      }
+    }
+    i++;
   }
   va_end(arg);
   return (strlen(format));
@@ -29,7 +85,7 @@ int   ft_printf(const char *format, ...)
 int main()
 {
   char *p = "abc";
-  printf("|%-5s|\n", p);
+  ft_printf("|%5s|\n", p);
   return 0;
 }
 
