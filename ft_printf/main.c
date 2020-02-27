08465f0638a12af6ade02009105f87988516f782
leocarneiro <leoramos_10@hotmail>
2020-02-27 17:14:30 -0300

    main.c

--- ft_printf/main.c
+++ ft_printf/main.c
@@ -13,11 +13,102 @@ void  ft_putstr(char *s)
   }
 }
 
+int   isnumber(char c)
+{
+  int   n;
+  if (c >= '0' && c <= '9')
+    n = 1;
+  else
+    n = 0;
+  return (n);
+}
+
+char  field_flags(const char *format)
+{
+  char  flag;
+  char  *f;
+  int   i;
+
+  f = (char *)format;
+  i = 0;
+  while (f[i])
+  {
+   if (f[i] == '-')
+     flag = '-';
+   else if (f[i] == '0')
+     flag = '0';
+   else
+     flag = 0;
+   i++;
+  }
+  return (flag);
+}
+
+int   field_width(const char *format)
+{
+  int   width;
+  
+  width = 0;
+  while (*format)
+  {
+    if (isnumber(*format) && *format != '0')
+    {
+      while (isnumber(*format))
+      {
+        width *= 10 + *format - 48;
+        format++;
+      }
+    }
+    format++;
+  }
+  return (width);
+}
+
+int   field_precision(const char *format)
+{
+  int   precision;
+
+  precision = 0;
+  while (*format)
+  {
+    if (*format == '.')
+    {
+      format++;
+      while (isnumber(*format))
+      {
+        precision *= 10 + *format - 48;
+        format++;
+      }
+    }
+    format++;
+  }
+  return (precision);
+}
+
+char   field_conversions(const char *format)
+{
+  char  *conv;
+  char  type;
+  int   i;
+
+  conv = "cspdiuxX%";
+  type = 0;
+  i = 0;
+  while (*conv)
+  {
+    if (strchr(format, *conv))
+    {
+      type = *format;
+      break ;
+    }
+    conv++;
+  }
+  return (type);
+}
 
 int   ft_printf(const char *format, ...)
 {
   va_list   arg;
-  char      *fields;
   char      *s;
   int       i;
   int       j;
@@ -26,35 +117,7 @@ int   ft_printf(const char *format, ...)
 
   va_start(arg, format);
   s = va_arg(arg, char *);
-  /*
-  fields = (char *)malloc(10);
-  //%[flags][width][.precision][size]type
-  while (format[i])
-  {
-    if (format[i] != '%')
-      write(1, &format[i], 1);
-    else
-    {
-      i++;
-      while (format[i] == '-')
-        fields[0] = '-';
-      while (format[i] >= '0' && format[i] <= '9')
-        fields[1] = format[i];
-      while (format[i] == 's')
-        fields[3] = '-';
-    }
-    i++;
-  }
-  len = 10;
   i = 0;
-  while (i < len)
-  {
-    while (s)
-      write(1, &s[i], 1);
-    write(1, " ", 1);
-    i++;
-  }
-  */
   while (format[i])
   {
     if (format[i] != '%')
@@ -84,8 +147,21 @@ int   ft_printf(const char *format, ...)
 
 int main()
 {
+  /*
   char *p = "abc";
   ft_printf("|%5s|\n", p);
+  */
+  const char  *format;
+  char f;
+
+  format = "%-5.6s";
+  f = field_flags(format);
+  printf("%c", f);
+  
+
+
+
+
   return 0;
 }
 
